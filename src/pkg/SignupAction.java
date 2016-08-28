cfa5eb3fe26b03f5ab6a4a3d19eb4bd5e4b7a81d
Kamlesh.... Be Happy <Kamlesh.... Be Happy>
2016-08-29 00:20:29 +0530

    adding TC

--- src/pkg/SignupAction.java
+++ src/pkg/SignupAction.java
@@ -31,22 +31,22 @@ public class SignupAction {
 		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		driver.get("http://www.appdirect.com/");
 		driver.manage().window().maximize();
-		 String pageTitle= driver.findElement(By.xpath("html/body/main/section[1]/div[2]/div/h1")).getText();
-       
-		 try
-		 {
-			 Assert.assertEquals(pageTitle, "The leading commerce platform for selling cloud services.");
-			 System.out.println("Assertion Pass");
-			 
-		 }
-		 catch(AssertionError e)
-		 {
-			 System.out.println("Assertion failed");
-		 }
-		 }
-	
+		String pageTitle= driver.findElement(By.xpath("html/body/main/section[1]/div[2]/div/h1")).getText();
+
+		try
+		{
+			Assert.assertEquals(pageTitle, "The leading commerce platform for selling cloud services.");
+			System.out.println("Assertion Pass");
+
+		}
+		catch(AssertionError e)
+		{
+			System.out.println("Assertion failed");
+		}
+	}
+
 	//TC#2 Verify user is navigated to login page after clicking on 'Login' button.
-	
+
 	@Test(priority=1)
 	public void LoginPageopenedTest(){ 
 
@@ -57,11 +57,11 @@ public class SignupAction {
 		try {/*
 			String el =driver.findElement(By.xpath(".//*[@id='login-header']/nav/ul/li/a/span")).getText();
 			el.equalsIgnoreCase("AppDirect");
-			*/
-			WebElement el = (new WebDriverWait(driver, 10))
-					  .until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='login-header']/nav/ul/li/a/span")));
+		 */
+			WebElement el = (new WebDriverWait(driver,20))
+					.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='login-header']/nav/ul/li/a/span")));
 			el.getText().equalsIgnoreCase("AppDirect");
-			
+
 			System.out.println("Login page exists");
 			existsElement=true;
 		} catch (NoSuchElementException e) {
@@ -69,27 +69,75 @@ public class SignupAction {
 		}
 	}
 
-	
+
 	//TC#3 Verify user is navigated to Signup page after clicking on 'Signup' button from the login page.
-	
-		@Test(priority=1)
-		public void SignupPageOpenedTest(){ 
 
-			boolean existsElement;
+	@Test(priority=1)
+	public void SignupPageOpenedTest(){ 
 
-			WebElement un=driver.findElement(By.xpath(".//*[@id='id5']/fieldset/div[3]/menu/a"));
-			un.click();
-			try {
-				String el =driver.findElement(By.xpath(".//*[@id='ida']/fieldset/div[1]/h3")).getText();
-				el.equalsIgnoreCase("Sign Up");
-				
-				System.out.println("Signup page opened");
-				existsElement=true;
-			} catch (NoSuchElementException e) {
-				existsElement=false;
-			}
+		boolean existsElement;
+
+		WebElement un=driver.findElement(By.xpath(".//*[@id='id5']/fieldset/div[3]/menu/a"));
+		un.click();
+		try {
+			String el =driver.findElement(By.xpath(".//*[@id='ida']/fieldset/div[1]/h3")).getText();
+			el.equalsIgnoreCase("Sign Up");
+
+			System.out.println("Signup page opened");
+			existsElement=true;
+		} catch (NoSuchElementException e) {
+			existsElement=false;
 		}
-	
+	}
+
+
+
+
+
+	//TC#4 Verify that place holder "email@address.com" is displayed in email field.
+
+	@Test(priority=1)
+	public void PlaceholderTest(){ 
+        String pla;
+		/*WebElement placeholder1 =driver.findElement(By.xpath(".//*[@id='ida']/fieldset/div[2]/div/input"));
+		placeholder1.getAttribute(placeholder);
+		 */
+		WebElement placeholder1= (new WebDriverWait(driver,20))
+				.until(ExpectedConditions.presenceOfElementLocated(By.className("adb-text__full_width")));
+		pla=placeholder1.getAttribute("placeholder");
+
+		try
+		{	
+			Assert.assertEquals(pla,"email@address.com");
+			System.out.println("Assertion Pass");
+
+		}
+		catch(AssertionError e)
+		{
+			System.out.println("Assertion failed");
+		}
+	}
+
+
+	//TC#5 Verify that message for activation link is displayed once user enters correct email ID.
+
+		@Test(priority=1)
+		public void ActivationLinkMessageTest() throws Exception{ 
+	        /*WebElement placeholder1 =driver.findElement(By.xpath(".//*[@id='ida']/fieldset/div[2]/div/input"));
+			placeholder1.getAttribute(placeholder);
+			 */
+			WebElement placeholder2= (new WebDriverWait(driver,20))
+					.until(ExpectedConditions.presenceOfElementLocated(By.className("adb-text__full_width")));
+			///ExcelReader obj =new ExcelReader();
+			//Read the email ID's from excel sheet
+			ExcelReader.setExcelFile(Constant.Path_TestData,"Sheet1");
+			String emailId = ExcelReader.getCellData(1,0);
+			System.out.println(emailId);
+			placeholder2.sendKeys(emailId);
+			driver.findElement(By.xpath(".//*[@id='idb']")).click();
+					}
+
+
 	
 	
 	/*		//TC#2 Verify if the user is navigated to registration page
@@ -181,5 +229,5 @@ public class SignupAction {
 		}
 		driver.close();
 
-*/	}
+	 */	}
 
